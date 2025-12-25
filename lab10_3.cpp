7558720bcc0c7ee94d19245f96d3ae3a50f963f4
Farm56 <sstg3455@gmail.com>
2025-12-25 13:40:32 +0700

    complete_lab10_3.cpp

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,31 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    int count = 0;
+    double sum = 0;
+    double sum_sq = 0;
+    double num;
+
+    ifstream source("score.txt");
+    while (source >> num) {
+        sum += num;
+        sum_sq += num * num;
+        count++;
+    }
+    source.close();
+
+    double mean = sum / count;
+    double std_dev = sqrt((sum_sq / count) - (mean * mean));
+
+    cout << "Number of data = " << count << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << std_dev << endl;
+
+    return 0;
+}
